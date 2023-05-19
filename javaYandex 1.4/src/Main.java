110c1aebc6cdc8f23794f67d53583d6c35e8238a
akmurza <ricomuerto666@gmail.com>
2023-05-19 21:05:40 +0400

    Initial commit

---
+++ javaYandex 1.4/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int c=4;
+        if (c>3 && c>2 && c >1 || c!=89)
+            System.out.println("more");
+        else
+            System.out.println("less");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("da");
+        }
+        int b=3;
+        System.out.println(b+1);
+        System.out.println(b+2);
+        int a=3;
+        System.out.println(++a);
+        System.out.println(a);
+//коварство инкремента первое выражение с b подтверждает что в примитиве все передается по значению, не вмешиваясь в изначальную
+//переменную
+//а второе выражение с а показывает, что инкремент преобразует саму переменную(как будто это ссылочный тип)
+
+
+    }
+}
